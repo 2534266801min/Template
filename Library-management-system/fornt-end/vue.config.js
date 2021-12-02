8438a11cd29bc2ace058cc12d566260d8ec252f0
SNiFe <32757525+2534266801min@users.noreply.github.com>
2021-12-02 23:02:17 +0800

    repo init

---
+++ Library-management-system/fornt-end/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    publicPath: './',
+    assetsDir: 'static',
+    productionSourceMap: false,
+    // devServer: {
+    //     proxy: {
+    //         '/api':{
+    //             target:'http://www.yanzx.top/server/api',
+    //             changeOrigin:true,
+    //             pathRewrite:{
+    //                 '^/api':''
+    //             }
+    //         }
+    //     }
+    // }
+}
